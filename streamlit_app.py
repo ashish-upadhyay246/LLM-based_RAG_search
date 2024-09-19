8e5959303bd5ed27a885ad0ba0bafb72a8f1dfb0
Ashish Upadhyay <upadhyay.ashish246@gmail.com>
2024-09-19 14:05:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,20 +12,20 @@ click=st.button("Enter", on_click=None, type="secondary", disabled=False, use_co
 message_placeholder=st.empty()
 
 if click and user_input:
-    message_placeholder.text("Generating response, please wait...")
-    #send POST request to Flask API
-    try:
-        response = requests.post('http://localhost:5000/process', json={"input": user_input,"sites_required": sites_required}, timeout=250)
-        response.raise_for_status()
-        if response.status_code == 200:
-            result = response.json()  #parse JSON response from Flask
+    with st.spinner('Generating response. Please wait...'):
+        #send POST request to Flask API
+        try:
+            response = requests.post('http://localhost:5000/process', json={"input": user_input,"sites_required": sites_required}, timeout=250)
+            response.raise_for_status()
+            if response.status_code == 200:
+                result = response.json()  #parse JSON response from Flask
 
-            message_placeholder.empty()
-            st.write("Result:")
-            st.write(result.get("result", "No result returned"))
-        else:
-            st.error(f"API returned an error: {response.status_code}")
-            st.write(response.text)
+                message_placeholder.empty()
+                st.write("Result:")
+                st.write(result.get("result", "No result returned"))
+            else:
+                st.error(f"API returned an error: {response.status_code}")
+                st.write(response.text)
 
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error: {e}")
+        except requests.exceptions.RequestException as e:
+            st.error(f"Error: {e}")
